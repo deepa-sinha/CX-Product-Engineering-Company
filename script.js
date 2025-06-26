4e3819e4b6408f172ed18dc0f8b3ecb9e75bf276
Deepa Sinha <deepa2017s@gmail.com>
2025-06-26 10:45:27 +0530

    fixed responsive for mobile

--- script.js
+++ script.js
@@ -29,3 +29,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const accordionHeadings = document.querySelectorAll('.product-heading');
+
+    accordionHeadings.forEach(heading => {
+        heading.addEventListener('click', () => {
+            const accordionProduct = heading.closest('.accordion-products');
+            const description = accordionProduct.querySelector('.product-description');
+            const svgIcon = heading.querySelector('svg');
+            const pathElement = svgIcon.querySelector('path');
+
+            description.classList.toggle('hidden');
+
+            const minusPath = 'M19 12.998H5V10.998H19V12.998Z';
+            const plusPath = 'M11 19V13H5V11H11V5H13V11H19V13H13V19H11Z';
+
+            if (svgIcon.classList.contains('plus')) {
+                pathElement.setAttribute('d', minusPath);
+                svgIcon.classList.remove('plus');
+                svgIcon.classList.add('minus');
+            } else if (svgIcon.classList.contains('minus')) {
+                pathElement.setAttribute('d', plusPath);
+                svgIcon.classList.remove('minus');
+                svgIcon.classList.add('plus');
+            }
+        });
+    });
+});
